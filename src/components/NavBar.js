900715c8b73da5c8a16d4f399ca958c5bea4bbcd
Zachary Elliott <zacharyberdell@gmail.com>
2022-01-28 11:30:21 -0700

    Comments and extra logic added to the NavBar component.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-function NavBar() {
+function NavBar({ currentPage, handlePageChange }) {
+    //Array with objects that are used to render the anchor tags
     const links = [{title: 'About Me', href: 'about-me'}, 
     {title: 'Portfolio', href: 'portfolio'},
     {title: 'Contact Me', href: 'contact-me'},
     {title: "Resume", href: 'resume'}];
 
+    //Renders the nav bar 
     return (
         <nav>
+            {/* Creates an anchor tag for each item in the array and adds logic to make it render for the current page */}
             {links.map(link => {
-                <a href={link.href} className={currentPage === link.title ? 'nav-link active' : 'nav-link'}>{link.title}</a>
+                <a href={link.href} 
+                className={currentPage === link.title ? 'nav-link active' : 'nav-link'}
+                onClick={() => handlePageChange(link.title)}>
+                    {link.title}
+                </a>
             })}
         </nav>
     );
 }
+
+export default NavBar;
