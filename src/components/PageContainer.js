76459034838a9c6eb55b414ef49b4e526bfcfc3d
Zachary Elliott <zacharyberdell@gmail.com>
2022-01-28 11:49:18 -0700

    Cleaned up file structure and Added functions for PageContainer component.

---
+++ src/components/PageContainer.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import Header from './Header';
+import AboutMe from './pages/AboutMe';
+import Portfolio from './pages/Portfolio';
+import ContactMe from './pages/ContactMe';
+import Resume from './pages/Resume';
+
+function PageContainer() {
+    const [currentPage, setCurrentPage] = useState('AboutMe');
+
+    const renderPage = () => {
+        switch(currentPage) {
+            case 'AboutMe': 
+                return <AboutMe />;
+            case 'Portfolio':
+                return <Portfolio />;
+            case 'ContactMe':
+                return <ContactMe />;
+            case 'Resume':
+                return <Resume />;
+            default:
+                console.log('There was an error rendering the page.');      
+        }
+    }
+
+    const handlePageChange = page => setCurrentPage(page);
+
+}
