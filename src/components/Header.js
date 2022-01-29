a4a5e36313f7576e0ea3904e49a0b1cb304c9684
Zachary Elliott <zacharyberdell@gmail.com>
2022-01-29 10:41:38 -0700

    Wrote the about me section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import NavBar from './NavBar';
 
-function Header() {
+function Header(currentPage, handlePageChange) {
 
     return (
         <header>
             <h1>Zachary Elliott</h1>
-            <NavBar />
+            <NavBar currentPage={currentPage} handlePageChange={handlePageChange}/>
         </header>
     )
 }
+
+export default Header;
