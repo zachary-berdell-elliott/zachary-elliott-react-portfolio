579c7d2a5bbae2a0cb57380dd818eee3e9fff69e
Zachary Elliott <zacharyberdell@gmail.com>
2022-01-30 13:26:21 -0700

    HTML for the contact link built.

--- src/components/pages/ContactMe.js
+++ src/components/pages/ContactMe.js
@@ -3,7 +3,16 @@ import React from 'react';
 function ContactMe() {
     return(
         <main>
-            
+            <h2>Contact Me</h2>
+            <form ariaLabel="contact form">
+                <label for="user-name">Your Name</label>
+                <input type="text" id="user-name" autocomplete="name" required placeholder="Enter your name"/>
+                <label for="user-email">Your Email</label>
+                <input type="text" id="user-email" autocomplete="email" required placeholder="Enter an email address"/>
+                <label for="user-message">Message</label>
+                <input type="text" id="user-message" required placeholder="Type your message here"/>
+                <button id="submit-btn">Submit</button>
+            </form>
         </main>
     );
 }
