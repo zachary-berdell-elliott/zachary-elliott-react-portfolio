c943200e7d82810b6ffc71bd70a54f4ece2c133b
Zachary Elliott <zacharyberdell@gmail.com>
2022-01-31 10:45:28 -0700

    Trying to get app to render.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import PageContainer from './components/PageContainer';
 
-const App = () => <PageContainer />;
+const App = () => {
+    return (
+        <>
+            <PageContainer />
+        </>
+    );
+}
 
 export default App;
