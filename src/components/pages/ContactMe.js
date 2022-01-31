7cc64b8eea99a3672197ccec516e8c8a65a8bb17
Zachary Elliott <zacharyberdell@gmail.com>
2022-01-31 10:27:14 -0700

    Needed functions for checking users input added to the form.

--- src/components/pages/ContactMe.js
+++ src/components/pages/ContactMe.js
@@ -1,10 +1,31 @@
 import React from 'react';
 
 function ContactMe() {
+    const [input, setInput] = useState('');
+
+    //Function for handling form submission
+    const handleSubmit = (e) => {
+        e.preventDefault;
+
+
+    }
+
+    const nameCheck = (e) => {
+
+    }
+
+    const emailCheck = (e) => {
+
+    }
+
+    const messageCheck = (e) => {
+        
+    }
+
     return(
         <main>
             <h2>Contact Me</h2>
-            <form ariaLabel="contact form">
+            <form ariaLabel="contact form" onSubmit={handleSubmit}>
                 <label for="user-name">Your Name</label>
                 <input type="text" id="user-name" autocomplete="name" required placeholder="Enter your name"/>
                 <label for="user-email">Your Email</label>
