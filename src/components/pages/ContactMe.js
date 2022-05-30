156d5704f9d521adb1036c1a50e654620eb0ed8d
zachary <zacharyberdell@gmail.com>
2022-05-29 18:40:52 -0600

    Added most front end logic for the form.

--- src/components/pages/ContactMe.js
+++ src/components/pages/ContactMe.js
@@ -16,10 +16,45 @@ function ContactMe() {
     const [feedback, setFeedback] = useState('');
 
     //Function for handling form submission
-    const handleSubmit = (event) => {
-        //e.preventDefault;
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        var submitable = true;
 
+        if(!fieldEngaged.emailEngaged) {
+            setErrorMessages({
+                ...errorMessages,
+                emailError: 'You can not submit this form until you have entered your email.'
+            });
+            submitable = false;
+        } else if(errorMessages.emailError !== '') {
+            submitable = false;
+        }
 
+        if(!fieldEngaged.nameEngaged) {
+            setErrorMessages({
+                ...errorMessages,
+                nameError: 'You can not submit this form with out entering a name.'
+            });
+            submitable = false;
+        } else if(errorMessages.nameError !== '') {
+            submitable = false;
+        }
+
+        if(!fieldEngaged.messageEngaged) {
+            setErrorMessages({
+                ...errorMessages,
+                messageError: 'You need to enter a message about what you are contacting for in order to submit this form.'
+            });
+            submitable = false;
+        } else if(errorMessages.messageError !== '') {
+            submitable = false;
+        }
+
+        if(submitable) {
+            setFeedback('Your response has been recorded. I will get back to you when possible.');
+        } else {
+            setFeedback('You appear to have made a mistake when filling out the form. please review in order to find what your mistake is.');
+        }
     }
 
     const messageCheck = (e) => {
@@ -65,9 +100,9 @@ function ContactMe() {
     }
 
     const nameCheck = (e) => {
-        !e.target.length ? setErrorMessages({
+        !e.target.value.length ? setErrorMessages({
             ...errorMessages,
-            nameError: 'Please eneter a name to submit this form.'
+            nameError: 'Please enter your name here'
         }) : setErrorMessages({
             ...errorMessages,
             nameError: ''
@@ -78,18 +113,20 @@ function ContactMe() {
             nameEngaged: true
         });
     }
-    
+
     return(
         <main>
             <h2>Contact Me</h2>
             <form aria-label="contact form" onSubmit={handleSubmit} id="contact-form">
                 <label htmlFor="user-name">Your Name <span className="required">*</span></label>
                 <input type="text" id="user-name" autoComplete="name" required placeholder="Enter your name" onBlur={nameCheck}/>
-
+                <div className="required">{errorMessages.nameError}</div>
                 <label htmlFor="user-email">Your Email <span className="required">*</span></label>
                 <input type="text" id="user-email" autoComplete="email" required placeholder="Enter an email address"  onBlur={emailCheck} />
+                <div className="required">{errorMessages.emailError}</div>
                 <label htmlFor="user-message">Message <span className="required">*</span></label>
                 <textarea id="user-message" required placeholder="Type your message here" onBlur={messageCheck}/>
+                <div className="required">{errorMessages.messageError}</div>
                 <button id="submit-btn">Submit</button>
             </form>
             <p className="feedback-text">{feedback}</p>
